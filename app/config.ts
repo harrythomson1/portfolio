03aa9acdbd8b1f9a9f1229643800e517b8207338
Harry Thomson <harrythomson@harrys-mbp.home>
2025-03-10 12:38:34 +0000

    Remove social links

--- app/config.ts
+++ app/config.ts
@@ -8,9 +8,7 @@ export const metaData = {
 };
 
 export const socialLinks = {
-  twitter: "https://x.com/1tssirius",
-  github: "https://github.com/1msirius/Nextfolio",
-  instagram: "https://www.instagram.com/",
-  linkedin: "https://www.linkedin.com/",
-  email: "mailto:example@gmail.com",
+  github: "https://github.com/harrythomson1",
+  linkedin: "https://www.linkedin.com/in/harry-thomson-536674211/",
+  email: "haroldt95@hotmail.com",
 };
