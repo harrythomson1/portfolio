207e180adc6682bd4e2d5943d7a7cb02a9f5a65a
Harry Thomson <haroldt95@hotmail.com>
2025-03-10 15:44:00 +0000

    Remove project that was no mine

--- app/projects/project-data.tsx
+++ app/projects/project-data.tsx
@@ -11,11 +11,5 @@ export const projects: Project[] = [
     year: 2021,
     description: "A 2D Ruby platformer",
     url: "/projects/super-ruby-bros",
-  },
-  {
-    title: "OpenDeepLearning",
-    year: 2023,
-    description: "Open source AI education resources",
-    url: "https://opendeeplearning.xyz/",
-  },
+  }
 ];
