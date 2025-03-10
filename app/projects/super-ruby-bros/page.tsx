f39663a1203f20a150909575dd13e461d606d6bc
Harry Thomson <haroldt95@hotmail.com>
2025-03-10 17:32:33 +0000

    Improve super ruby bros project page

--- app/projects/super-ruby-bros/page.tsx
+++ app/projects/super-ruby-bros/page.tsx
@@ -12,10 +12,10 @@ export default function SuperRubyBros() {
         className="pb-4"
       />
       <p className="pb-4">Super Ruby Bros is a 2D platformer built in Ruby using Ruby2D, featuring physics-based movement, collision detection, and basic AI behaviors. While Ruby isn’t a conventional choice for game development, this project challenged me to implement game loops, sprite animations, and object interactions from scratch.</p>
-      <p>Through this project, I gained experience in event-driven programming, optimizing real-time updates, and structuring code for interactive applications. It was a pivotal step in my development journey, reinforcing my problem-solving skills and deepening my understanding of how game engines handle state and logic.</p>
-      <p><strong>Technology Used:</strong></p>
+      <p className="pb-4">Through this project, I gained experience in event-driven programming, optimizing real-time updates, and structuring code for interactive applications. It was a pivotal step in my development journey, reinforcing my problem-solving skills and deepening my understanding of how game engines handle state and logic.</p>
+      <p><strong>Technologies Used:</strong></p>
       <p><strong>Ruby</strong> - Core logic and object-oriented structure</p>
-      <p><strong>Ruby2D</strong> - Rendering, sprite management, and input handling</p>
+      <p className="pb-4"><strong>Ruby2D</strong> - Rendering, sprite management, and input handling</p>
       <p><strong>GitHub:</strong> <a href="https://github.com/harrythomson1/super-ruby-bros" className="underline">Super Ruby Bros</a></p>
     </>
   );
